ca76d313bd3512bc7309d7f27d79b65827bbcad5
omer neziroglu <omerneziroglu@gmail.com>
2021-01-23 20:49:50 +0300

    MyDictionaryOdevi

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] keys;
+        V[] values;
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+
+        }
+        public void Add(K key, V value)
+        {
+            K[] tempArray1 = keys;
+            keys = new K[keys.Length + 1];
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                keys[i] = tempArray1[i];
+            }
+
+            keys[keys.Length - 1] = key;
+
+            V[] tempArray2 = values;
+            values = new V[values.Length + 1];
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                values[i] = tempArray2[i];
+            }
+
+            values[values.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Ali");
+
+
+            
+        }
+    }
+}
